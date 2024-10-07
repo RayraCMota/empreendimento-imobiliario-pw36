f63974a565f01afe531d3e9df9725314622a1b38
RayraCMota <rayracmota@hotmail.com>
2024-10-07 16:56:24 -0300

    primeiro commit

---
+++ pw36/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('#carousel-imagens').slick({
+        autoplay: true
+    });
+
+    $('#carousel-imagens2').slick({
+        autoplay: true
+    });
+
+    $('#telefone').mask('(00)00000-0000');
+
+})
